753fb4143e532dca8e00dd0728a8d707b37dea10
David McKinley <dmckinley3@studentmail.nait.ca>
2022-01-08 11:58:23 -0700

    class created with major errors and missing code

---
+++ ClassPractice/ClassPractice/car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    internal class car
+    {
+        //fields//
+        private string _CourseNo;
+        private string _CourseName;
+        private string[] students;
+       
+        //properties
+        public string CourseNo
+        {
+            get { return _CourseNo; }
+            private set { _CourseNo = value; }  
+        }
+
+        //constructors
+
+        //methods
+    }
+}
---
+++ OOPDemo01/OOPDemo01/Course.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo01
+{
+    public class Course
+    {
+
+        //fields//
+        private string _CourseNo;
+        private string _CourseName;
+        private string[] students;
+
+        //properties
+        public string CourseNo { get; set; }
+        public string CourseName { get; set; }
+    }
+
+    //constructor
+    public Course(string courseNo, string courseName)
+    {
+        CourseNo = courseNo;
+        CourseName = courseName;
+        
+    }
+
+    //methods
+    public string AddStudent(string student)
+    {
+        return student;
+    }
+    public string DropStudent(string student)
+    {
+        return student;
+    }
+}
+
